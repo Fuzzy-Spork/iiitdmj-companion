521c030d5f374b662536bd85f149560a2aa17114
Sehej <sehejjain@outlook.com>
2020-07-07 15:42:33 +0530

    Dashboard Integration + new theme

---
+++ lib/constants.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+//const kBackgroundColor = Color(0xFF24252A);
+
+const kTextColor = Color(0xFF008CAB);
+
+const kCardColor = Color(0xFFFFFFFF);
+
+var kBackgroundColor = Color(0xFF008CAB);
+
+var kIconColor = Color(0xFF008CAB);
+
+const kFABColor = Colors.white;
+
+const kFacultyCardTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 15.0,
+    letterSpacing: 4,
+    fontFamily: 'SourceSansPro');
+
+const kSearchBarDecoration = InputDecoration(
+  filled: false,
+  labelText: "Search",
+  labelStyle: TextStyle(color: Colors.white),
+  hintText: "Search",
+  hintStyle: TextStyle(color: Colors.white70),
+  prefixIcon: Icon(
+    Icons.search,
+    color: Colors.white,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
